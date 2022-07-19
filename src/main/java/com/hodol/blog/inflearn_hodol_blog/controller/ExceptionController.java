36072060b27000259dc78a8828099d57b585851d
dev-owner <yureka1112@gmail.com>
2022-07-19 23:36:36 +0900

    use ControllerAdvice to abstract exception code

---
+++ src/main/java/com/hodol/blog/inflearn_hodol_blog/controller/ExceptionController.java
@@ -0,0 +1,39 @@
+package com.hodol.blog.inflearn_hodol_blog.controller;
+
+import com.hodol.blog.inflearn_hodol_blog.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Slf4j
+@ControllerAdvice
+public class ExceptionController {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public ErrorResponse invalidRequestHandler(MethodArgumentNotValidException e) {
+
+        ErrorResponse errorResponse = new ErrorResponse("400", "잘못된 요청 입니다.");
+        for (FieldError fieldError : e.getFieldErrors()) {
+            errorResponse.addValidation(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return errorResponse;
+    }
+
+//        // MethodArgumentNotValidException
+//        log.error("Exception handler error", e);
+//        FieldError fieldError = e.getFieldError();
+//        String field = fieldError.getField();
+//        String defaultMessage = fieldError.getDefaultMessage();
+//
+//        Map<String, String> response = new HashMap<>();
+//        response.put(field, defaultMessage);
+//        return response;
+
+}
--- src/main/java/com/hodol/blog/inflearn_hodol_blog/controller/PostController.java
+++ src/main/java/com/hodol/blog/inflearn_hodol_blog/controller/PostController.java
@@ -38,7 +38,8 @@ public class PostController {
 
     @PostMapping("/posts")
 //    public String post(@RequestParam Map<String, String> params) {
-    public Map<String, String> post(@RequestBody @Valid PostCreate params, BindingResult result) throws Exception {
+//    public Map<String, String> post(@RequestBody @Valid PostCreate params, BindingResult result) throws Exception {
+    public Map<String, String> post(@RequestBody @Valid PostCreate params) throws Exception {
 //        log.info("title={}, content={}", title, content);
         log.info("params={}", params);
 
@@ -54,7 +55,14 @@ public class PostController {
         String content = params.getContent();
 
         // 잘못 들어온다면, response를 만들고 싶다.
-        if (result.hasErrors()) {
+        // 이것도 좋지만.. 매번 메서드마다 검증을 해야한다.
+        //  > 까먹을 수 있고, 지겹다, 버그가 발생할 여지가 높다.
+        // 응답 값에 hashmap > 응답 클래스를 만들어 주자
+        // 여러개의 에러처리가 힘들다
+        // 세 번 이상 반복작업은 피하자
+        // controlleradvice를 통해 개선해보자
+        // 모든 컨트롤러에 대한 에러를 처리
+       /* if (result.hasErrors()) {
             List<FieldError> fieldErrors = result.getFieldErrors();
             FieldError fieldError = fieldErrors.get(0);
             String invalidFieldName = fieldError.getField();
@@ -63,7 +71,7 @@ public class PostController {
             Map<String, String> error = new HashMap<>();
             error.put(invalidFieldName, errorMessage);
             return error;
-        }
+        }*/
         return Map.of();
     }
 
